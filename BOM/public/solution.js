7faa4ff22bc8013d128478296a20184eef8f60b2
Евгений Вульман <ezakharchev@yandex.ru>
2022-11-19 17:18:41 +0300

    new lesson BOM

---
+++ BOM/public/solution.js
@@ -0,0 +1,15 @@
+// BEGIN (write your solution here)
+const solution = (url) => {
+    window.location.href = url;
+    return `${navigator.appCodeName}/${navigator.appVersion} ${url}`;
+};
+// END
+
+// TEACHER SOLUTION
+export default (href) => {
+    window.location.href = href;
+    const { userAgent } = window.navigator;
+    const [agentName] = userAgent.split(' ');
+    return `${agentName} ${window.location.href}`;
+}
+// END

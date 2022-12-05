a07138d873ea000bcb001c512e470f5a590dc56d
Евгений Вульман <ezakharchev@yandex.ru>
2022-12-05 16:39:16 +0300

    new lesson Forms

---
+++ Forms/src/application.js
@@ -0,0 +1,29 @@
+import { htmlEscape } from "escape-goat";
+
+// BEGIN (write your solution here)
+const render = (element, data) => {
+    const div = document.createElement('div');
+    const { email, name, comment } = data;
+    div.innerHTML = `
+        <p>Feedback has been sent</p>
+        <div>Email: ${htmlEscape(email)}</div>
+        <div>Name: ${htmlEscape(name)}</div>
+        <div>Comment: ${htmlEscape(comment)}</div>
+    `;
+
+    element.replaceWith(div);
+};
+
+export default () => {
+    const formElement = document.querySelector('.feedback-form');
+
+    const  handle = (element) => {
+        element.preventDefault();
+        const formData = new FormData(element.target);
+
+        render(formElement, Object.fromEntries(formData));
+    };
+
+    formElement.addEventListener('submit', handle);
+};
+// END
